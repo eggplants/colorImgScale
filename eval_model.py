088cd807b50c56adad83c8fd78bb30a33ae12e40
eggplants <w10776e8w@yahoo.co.jp>
2021-06-30 06:15:01 +0900

    Add: README.md

--- eval_model.py
+++ eval_model.py
@@ -5,6 +5,7 @@ import sys
 import csv
 import os
 
+# Usage: python eval_model.py ../test/aozora_week210625.kv.bin 
 
 p=print
 if len(sys.argv) != 2:
@@ -32,8 +33,10 @@ p('model:', model)
 
 r=csv.reader(open('color_attribute_score_vocab.csv', 'r'))
 next(r) #header
-vs=[i for i in r]
-vs_exists=[i for i in vs if i[0] in model or i[1] in model]
+vs=set(tuple(i) for i in r)
+vs_exists=set(tuple(i) for i in vs if i[0] in model or i[1] in model)
 ls=(len(vs_exists), len(vs),)
 p('{}/{}'.format(*ls))
 p((ls[0]/ls[1])*100.0,"%")
+p('exists:    ', vs_exists)
+p('not exists:', vs-vs_exists)
